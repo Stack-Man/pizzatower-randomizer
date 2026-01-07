dc28bd4efcad0ca94cc8b6eaa49ff89d78911d15
Stack-Man <brandonjchristian@gmail.com>
2026-01-06 16:35:23 -0800

    construct level

--- layer_traversal.py
+++ layer_traversal.py
@@ -31,42 +31,99 @@ Branch path should choose a specific path.
 """
 class Level():
     def __init__(self):
-        self.start_segment = None
+        self.segments = [] #List of RoomSegment, PathSegment, and BranchSegment
         self.branch_count = 0
-        self.branch_segments = []
-        self.end_segment = None
-
-class StartSegment():
-    def __init__(self):
-        self.entrance = None
-        self.is_branch_start = False
-        self.OW_NPT = None
-        self.OW_PT = None
-        self.end = None
-        self.is_john_end = False
 
 class BranchSegment():
     def __init__(self):
-        self.TW = None
-        self.BS = None
-        self.OW_NPT = None
-        self.OW_PT = None
-        self.BE = None
+        self.start_path = None #RoomPath
+        self.OW_NPT = None #PathSegment
+        self.OW_PT = None #PathSegment
+        self.end_path = None #RoomPath
+
+class PathSegment():
+    def __init__
+        self.paths = [] #List of RoomPath
+
+class RoomSegment():
+    def __init__
+        self.path = None #Singular RoomPath
 
 
-class EndSegment():
-    def __init__(self):
-        self.TW = None
-        self.BS = None
-        self.OW_NPT = None
-        self.OW_PT = None
-        self.end = None
-        self.is_branch_end = False
-
-
+#TODO: do all levels construct then go through and grow paths after
 def construct_level_from_layers(E, EBS, J, JBE, BS, BE, TW, OW_PT, OW_NPT):
+    level = Level()
     
-    Level = []
+    #choose start
+    start_segment = construct_start_segment(E, EBS, JBE, OW_PT, OW_NPT)
+    level.segments.append(start_segment)
+    
+    if isinstance(start_segment, BranchSegment):
+        level.branch_count = level.branch_count + 1
+    
+    max_branches = 1
+    prev_segment = start_segment
+    
+    #choose branches
+    while (level.branch_count < max_branches):
+        
+        A = get_end_endpoint(prev_segment)
+        twoway_segment, branch_segment = construct_two_way_to_branch_segment(BS, BE, OW_PT, OW_NPT, TW, A)
+        
+        level.append(twoway_segment)
+        level.append(branch_segment)
+        
+        level.branch_count = level.branch_count + 1
+        
+        prev_segment = branch_segment
+
+    #choose end
+    A = get_end_endpoint(prev_segment)
+    twoway_segment, end_segment = construct_two_way_to_end_segment(J, JBE, BS, OW_PT, OW_NPT, TW, A)
+    
+    level.append(twoway_segment)
+    level.append(end_segment)
+    
+    if isinstance(end_segment, BranchSegment):
+        level.branch_count = level.branch_count + 1
+
+    return level
+
+def get_end_endpoint(segment):
+    return "TODO get end endpoint"
+
+def construct_two_way_to_branch_segment(BS, BE, OW_PT, OW_NPT, TW, A):
+    #choose some BS
+    #get F from BS
+    
+    #find TW from A to F
+        #if cant, try new BS
+    #find B  from BS to BE
+        #if cant, ditch TW and try new BS
+    
+    #repeat with different BS until some A > TW > B is found
+        #if none, report back that the last segment is faulty
+    
+    #TODO: ditch_path to refund path from find_path
+    #TODO: include Endpoint types in RoomPath
+    
+    return
+
+def construct_two_way_to_end_segment(J, JBE, BS, OW_PT, OW_NPT, TW, A): #TODO: account for max branches
+    #choose some J (or JE)
+    #get F from J
+    
+    #TODO: use construct_two_way_to_branch_segment if JE
+    #find TW from A to F
+        #if cant, try new J or JE
+    #if JE find B  from BS to JE
+        #if cant, ditch TW and try new JE or J
+    
+    #repeat with different JE or J until some A > TW > B is found
+        #if none, report back that the last segment is faulty
+    
+    #TODO: ditch_path to refund path from find_path
+    #TODO: include Endpoint types in RoomPath
     
     return
 
@@ -84,7 +141,7 @@ def construct_start_segment(E, EBS, JBE, OW_PT, OW_NPT, ):
     
     return
 
-def construct_branch_segment(BS, BE, TW, OW_PT, OW_NPT):
+def construct_branch_segment(BS, BE, OW_PT, OW_NPT):
     """
     |-Branch Segments-|
     Two Way > Branch Start > One Way  > Branch End > ...
